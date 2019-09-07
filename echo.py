9f3243cceb7e1eae48754c6025e9115ac0a50261
Tamekia N <tamekialashay@outlook.com>
2019-09-07 17:12:15 -0400

    testing submission

--- echo.py
+++ echo.py
@@ -2,21 +2,48 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "tamekiaNelson"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description="Perform changes on input text.")
+    parser.add_argument(
+        "-u", "--upper", help="convert text to uppercase", action="store_true")
+    parser.add_argument(
+        "-l", "--lower", help="convert text to lowercase", action="store_true")
+    parser.add_argument(
+        "-t", "--title", help="convert text to titlecase", action="store_true")
+    parser.add_argument(
+        "text", help="text to be manipulated")
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    parser = create_parser()
+    if not args:
+        parser.print_usage()
+        sys.exit()
+    args = parser.parse_args(args)
+    text = args.text
+    if args.upper and args.lower and args.title:
+        text = text.title()
+    elif args.upper:
+        text = text.upper()
+    elif args.lower:
+        text = text.lower()
+    elif args.title:
+        text = text.title()
+
+    return text
 
 
 if __name__ == '__main__':
-    pass
+    results = main(sys.argv[1:])
+    print(results)
